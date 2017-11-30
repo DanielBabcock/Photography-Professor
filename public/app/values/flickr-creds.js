804d39e2e54933847bd43e7ca434b0ede499b250
DanielBabcock <babcock.daniel.b@gmail.com>
2017-11-30 15:18:57 -0600

    deployed

---
+++ public/app/values/flickr-creds.js
@@ -0,0 +1,19 @@
+// "use strict";
+// Photography Professor
+// Key:
+// 7a85d39c45a63d81796cae19005f1d2a
+
+// Secret:
+// 5b129e25396fc850
+
+
+// https://www.npmjs.com/package/flickrapi
+
+// https://www.flickr.com/services/api/
+
+// https://www.flickr.com/services/api/flickr.photos.getExif.html
+
+// flickr.urls.getUserPhotos
+
+// album link
+// https://www.flickr.com/gp/154655854@N07/DYR99X
